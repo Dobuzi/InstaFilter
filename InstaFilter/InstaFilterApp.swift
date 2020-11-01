78791518ed0c082d9ac5ea63f1b9399e2f1359c0
Dobuzi <kimjw7981@gmail.com>
2020-11-01 22:32:08 +0900

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by 김종원 on 2020/11/01.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
